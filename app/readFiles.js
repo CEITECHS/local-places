9877b005c3bb01c707324e0c55af90cedaba9b68
Iddy Magohe <iddyiam@gmail.com>
2017-03-10 23:46:32 -0500

    readFiles to return json responses

---
+++ app/readFiles.js
@@ -0,0 +1,45 @@
+"use strict"
+const fs = require('fs');
+const https = require('https');
+
+
+module.exports = {
+    readJson: (filename, mode, callback) => {
+        if (mode != "R") {
+            let path = process.cwd();
+            fs.readFile(path + filename, 'utf8', (err, data) => {
+                let parsed;
+                if (err)
+                    return callback(err);
+                try {
+                    parsed = JSON.parse(data);
+                } catch (err) {
+                    return callback(err);
+                }
+                // no errors, propagate just the data
+                callback(null, parsed);
+            });
+
+        } else {
+            let parsed;
+            https.get(filename).on('response', response => {
+                let data = '';
+                response.on('data', chunk => {
+                    data += chunk;
+                });
+                response.on('end', () => {
+                    try {
+                        parsed = JSON.parse(data);
+                    } catch (err) {
+                        return callback(err);
+                    }
+                    callback(null, parsed);
+                });
+                response.on('error', err => {
+                    return callback(err);
+                });
+            });
+        }
+    }
+
+};
---
+++ test/readFiles-test.js
@@ -0,0 +1,34 @@
+"use strict"
+const assert = require('assert');
+const readFiles = require('../app/readFiles');
+
+describe('ReadFile from Local', () => {
+    describe('#readJson()', () => {
+        it('should read without error', (done) => {
+            readFiles.readJson('/test/data.json',"L", (err, data) => {
+                if (err) done(err)
+                else {
+                    console.log(data);
+                    assert.equal(data.length, 1 ,"size is supposed to be 1");
+                    done();
+                }
+            })
+        });
+    });
+});
+
+
+describe('ReadFile from Remote', () => {
+    describe('#readJson()', () => {
+        it('should read without error', (done) => {
+            readFiles.readJson('https://raw.githubusercontent.com/CEITECHS/pango-configs/master/data.json',"R", (err, data) => {
+                if (err) done(err)
+                else {
+                    console.log(data);
+                    assert.equal(data.length, 1 ,"size is supposed to be 1");
+                    done();
+                }
+            })
+        });
+    });
+});
